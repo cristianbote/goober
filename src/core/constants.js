f30ce19a192c4055af2ac5c0984a017432a3d655
cristianbote <botecristian@yahoo.com>
2019-02-14 16:55:38 +0200

    Add a test case for unsupported structure

--- src/core/constants.js
+++ src/core/constants.js
@@ -4,7 +4,7 @@ const NEW_LINE = "\n";
 const TRIM_RULE = /(\s|\n)/gim;
 const NEW_LINES_SEL = /(\s{2,})/gm;
 const REGULAR_SEL = /^(\.|@|\*|&|#|[a-z]+\s{)+/gm;
-const COMMA_SEL = /([{|,|;])\n{2,}/gm;
+const COMMA_SEL = /([{|,])\n{2,}/gm;
 
 // Values
 const SHEET_ID = "data-goober";
--- src/core/parser/tests/normalizer.test.js
+++ src/core/parser/tests/normalizer.test.js
@@ -14,12 +14,53 @@ describe("normalizer", () => {
 
 
 
-            button,
             a {
                 color: random;
             }
             @media all {
                 block: display;
+            }
+        `);
+
+        expect(out).toEqual([
+            "",  // newline
+            "foo: baz;",
+            "prop: 1px 3px 0px rgba(1111, 1, 1, 1) inset;",
+            "", // newline
+            "&:hover,",
+            "&:focus {",
+            "nope: 1;",
+            "}",
+            "", // newline
+            "a {",
+            "color: random;",
+            "}",
+            "", // newline
+            "@media all {",
+            "block: display;",
+            "}",
+            ""  // newline
+        ].join("\n"));
+    });
+
+    xit("unsupported", () => {
+        const out = normalizer(`
+            foo: baz;
+            prop: 1px 3px 0px rgba(1111, 1, 1, 1) inset;
+
+            &:hover,
+            &:focus {
+                nope: 1;
+            }
+
+            button,
+            a {
+                color: random;
+            }
+            
+            @media all {
+                block: display;
+                
                 .class {
                     javascript: alert(1);
                 }
