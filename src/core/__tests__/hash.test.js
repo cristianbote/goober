0e7b0109bc51208adc6af452e5ce0023ad65d7fb
Cristian Bote <cristianbote@users.noreply.github.com>
2019-03-06 14:02:50 +0200

    This introduces the caching mechanism (#34)
    * 1.1.1
    
    * Added cache, without wipe
    
    * Added the smallest possible size constrain for cache

--- src/core/__tests__/hash.test.js
+++ src/core/__tests__/hash.test.js
@@ -1,18 +1,31 @@
 import { hash } from "../hash";
 import { toHash } from "../to-hash";
-import { generate } from "../generate";
 import { update } from "../update";
+import { parse } from "../parse";
+import { astish } from "../astish";
+
+jest.mock("../astish", () => ({
+    astish: jest.fn().mockReturnValue("astish()")
+}));
+
+jest.mock("../parse", () => ({
+    parse: jest.fn().mockReturnValue("parse()")
+}));
 
 jest.mock("../to-hash", () => ({
     toHash: jest.fn().mockReturnValue(".toHash()")
 }));
 
-jest.mock("../generate", () => ({
-    generate: jest.fn().mockReturnValue("generate()")
-}));
-
 jest.mock("../update", () => ({
     update: jest.fn().mockReturnValue("update()")
+}));
+
+jest.mock("../astish", () => ({
+    astish: jest.fn().mockReturnValue("astish()")
+}));
+
+jest.mock("../parse", () => ({
+    parse: jest.fn().mockReturnValue("parse()")
 }));
 
 describe("hash", () => {
@@ -20,26 +33,54 @@ describe("hash", () => {
     beforeEach(() => {
         toHash.mockClear();
         update.mockClear();
-        generate.mockClear();
+        parse.mockClear();
+        astish.mockClear();
     });
 
     it("regression", () => {
         const res = hash("compiled", "target");
 
         expect(toHash).toBeCalledWith("compiled");
-        expect(update).toBeCalledWith("generate()", "target");
-        expect(generate).toBeCalledWith(".toHash()", "compiled");
+        expect(update).toBeCalledWith("parse()", "target");
+        expect(astish).toBeCalledWith("compiled");
+        expect(parse).toBeCalledWith("astish()", ".toHash()");
+
+        expect(res).toEqual("toHash()");
+    });
+
+    it("regression: cache", () => {
+        const res = hash("compiled", "target");
+
+        expect(toHash).not.toBeCalled();
+        expect(astish).not.toBeCalled();
+        expect(parse).not.toBeCalled();
+        expect(update).toBeCalledWith("parse()", "target");
 
         expect(res).toEqual("toHash()");
     });
 
     it("regression: global", () => {
-        const res = hash("compiled", "target", true);
+        const res = hash("global", "target", true);
 
         expect(toHash).not.toBeCalled();
-        expect(update).toBeCalledWith("generate()", "target");
-        expect(generate).toBeCalledWith("", "compiled");
+        expect(astish).toBeCalledWith("global");
+        expect(parse).toBeCalledWith("astish()", "");
+        expect(update).toBeCalledWith("parse()", "target");
 
         expect(res).toEqual("");
     });
+
+    it("regression: object", () => {
+        const className = Math.random() + "unique";
+        toHash.mockReturnValue(className);
+
+        const res = hash({ baz: 1 }, "target");
+
+        expect(toHash).toBeCalledWith({ baz: 1 });
+        expect(astish).not.toBeCalled();
+        expect(parse).toBeCalledWith({ baz: 1 }, className);
+        expect(update).toBeCalledWith("parse()", "target");
+
+        expect(res).toEqual(className.substr(1));
+    })
 });
