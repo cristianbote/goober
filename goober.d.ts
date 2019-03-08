2653c0264294e97abe6c1678ad96505866d5acab
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-03-08 11:07:35 -0800

    Fixed typings for TemplateStrings.
    Added csstype as a devDependency

--- goober.d.ts
+++ goober.d.ts
@@ -8,9 +8,9 @@ declare namespace goober {
     function styled (tag: string): tagged;
     function setPragma (val: StyledVNode): void;
     function extractCSS (): string;
-    function css (tag: CSSAttribute | string): ClassName;
+    function css (tag: CSSAttribute | TemplateStringsArray): ClassName;
     type StyledVNode = (props: any, ...args: any[]) => any;
-    type tagged = (tag: string | CSSAttribute) => StyledVNode;
+    type tagged = (tag: CSSAttribute | TemplateStringsArray) => StyledVNode;
     interface CSSAttribute extends CSSPropertiesHyphen {
         [key: string]: CSSPropertiesHyphen | string | number;
     }
