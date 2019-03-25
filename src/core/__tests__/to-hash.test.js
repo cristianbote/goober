415838d0be0c1695870913380167b64ecc43b110
cristianbote <botecristian@yahoo.com>
2019-03-25 23:01:22 +0200

    Bump the size adjust the hash fn

--- src/core/__tests__/to-hash.test.js
+++ src/core/__tests__/to-hash.test.js
@@ -4,7 +4,7 @@ describe("to-hash", () => {
     it("regression", () => {
         const res = toHash("goober");
 
-        expect(res).toEqual(".g0638");
-        expect(toHash("goober")).toEqual(".g0638");
+        expect(res).toEqual(".g01240249687");
+        expect(toHash("goober")).toEqual(".g01240249687");
     });
 });
--- src/core/to-hash.js
+++ src/core/to-hash.js
@@ -3,7 +3,7 @@
  * @param {String} str 
  */
 export const toHash = (str) => (
-    ".g0" + str
+    ".g0" + ~str
         .split("")
-        .reduce((out, i) => (out + i.charCodeAt(0)), 0)
+        .reduce((out, i) => ((out << 5) - out + i.charCodeAt(0)), 0)
 );
