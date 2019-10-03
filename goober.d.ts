b27767a68a4ed5d100fdca7d30be64e1755cdce6
Peter <10147685+pmkroeker@users.noreply.github.com>
2019-10-03 07:32:01 -0700

    Properly place type generics
    Passing generics into types need to be placed differently. I have moved them to where they should be.

--- goober.d.ts
+++ goober.d.ts
@@ -5,12 +5,12 @@ export = goober;
 export as namespace goober;
 
 declare namespace goober {
-    function styled <T = any>(tag: string): tagged<T>;
+    function styled <T = {}>(tag: string): tagged<T>;
     function setPragma (val: StyledVNode): void;
     function extractCss (): string;
     function css (tag: CSSAttribute | TemplateStringsArray): ClassName;
-    type StyledVNode = <T>(props: T, ...args: any[]) => any;
-    type tagged = <T>(tag: CSSAttribute | TemplateStringsArray) => StyledVNode<T>;
+    type StyledVNode<T = {}> = (props: T, ...args: any[]) => any;
+    type tagged<T = {}> = (tag: CSSAttribute | TemplateStringsArray) => StyledVNode<T>;
     interface CSSAttribute extends CSSProperties {
         [key: string]: CSSAttribute | string | number;
     }
