f591352b067608a02e331ed4147ced1cd5d4ab6f
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-10-10 08:10:39 -0700

    Fix typings to allow using props in tagged.

--- goober.d.ts
+++ goober.d.ts
@@ -9,8 +9,8 @@ declare namespace goober {
     function setPragma <T>(val: T): void;
     function extractCss (): string;
     function css (tag: CSSAttribute | TemplateStringsArray): ClassName;
-    type StyledVNode<T = {}> = (props: T, ...args: any[]) => any;
-    type tagged<T = {}> = (tag: CSSAttribute | TemplateStringsArray) => StyledVNode<T>;
+    type StyledVNode<T> = (props: T, ...args: any[]) => any;
+    type tagged<T> = (tag: CSSAttribute | TemplateStringsArray, props: (props: T) => CSSAttribute | string | number | undefined) => StyledVNode<T>;
     interface CSSAttribute extends CSSProperties {
         [key: string]: CSSAttribute | string | number | undefined;
     }
