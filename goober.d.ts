016446673f4c2f9c53d36947108dd00a2c8244f6
cristianbote <botecristian@yahoo.com>
2019-11-22 22:36:18 +0200

    Added glob types (Fixes #57)

--- goober.d.ts
+++ goober.d.ts
@@ -8,6 +8,7 @@ declare namespace goober {
     function styled <T = {}>(tag: string): tagged<T>;
     function setPragma <T>(val: T): void;
     function extractCss (): string;
+    function glob (tag: CSSAttribute | TemplateStringsArray): void;
     function css (tag: CSSAttribute | TemplateStringsArray): ClassName;
     type StyledVNode<T> = (props: T, ...args: any[]) => any;
     type tagged<T> = (tag: CSSAttribute | TemplateStringsArray, props: (props: T) => CSSAttribute | string | number | undefined) => StyledVNode<T>;
