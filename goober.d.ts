c526f35f8719c867d884412e5d7788c1c87d88c3
Martin Hochel <hochelmartin@gmail.com>
2019-12-10 15:03:43 +0100

    Improve type definitions (#65)
    * build: serve proper type definition file in prod
    
    * feat(types): improve types and add better test coverage

--- goober.d.ts
+++ goober.d.ts
@@ -5,15 +5,15 @@ export = goober;
 export as namespace goober;
 
 declare namespace goober {
-    function styled <T = {}>(tag: string): tagged<T>;
+    function styled <T = {}>(tag: string): Tagged<T>;
     function setPragma <T>(val: T): void;
     function extractCss (): string;
-    function glob (tag: CSSAttribute | TemplateStringsArray): void;
-    function css (tag: CSSAttribute | TemplateStringsArray): ClassName;
+    function glob (tag: CSSAttribute | TemplateStringsArray | string): void;
+    function css<T extends unknown = void>(tag: TemplateStringsArray, props?: (props: T) => string): CssFactory<T>
     type StyledVNode<T> = (props: T, ...args: any[]) => any;
-    type tagged<T> = (tag: CSSAttribute | TemplateStringsArray, props: (props: T) => CSSAttribute | string | number | undefined) => StyledVNode<T>;
+    type Tagged<T> = (tag: CSSAttribute | TemplateStringsArray, props?: (props: T) => CSSAttribute | string | number | undefined) => StyledVNode<T>;
     interface CSSAttribute extends CSSProperties {
         [key: string]: CSSAttribute | string | number | undefined;
     }
-    type ClassName = string;
+    type CssFactory<T> = (props:T) => string;
 }
---
+++ ts-tests/test-api.tsx
@@ -0,0 +1,73 @@
+import { h, ComponentChildren } from 'preact';
+import { styled, setPragma, css, glob } from '../goober';
+
+setPragma(h);
+
+const testStyledCss = () => {
+  interface ButtonProps {
+    clicked: boolean;
+    children: ComponentChildren
+    color?: string;
+  }
+
+  const buttonStyles = css<Pick<ButtonProps,'color'>>`
+    background: ${props=>props.color || 'black'};
+  `;
+
+  const buttonStylesRaw = css`
+    background: black;
+  `
+
+
+  const Button = styled<ButtonProps>('button')`
+      background: ${props => (props.color ? props.color : 'black')};
+  `;
+
+  const ButtonRaw = styled<ButtonProps>('button')`
+      background: black;
+  `;
+
+  const TestComp = () => {
+    return (
+        <div>
+            <Button clicked={false} color="red">click me</Button>
+            <ButtonRaw clicked={false}>click me</ButtonRaw>
+            <button class={buttonStyles({color:'red'})}>click me</button>
+            <button class={buttonStylesRaw()}>click me</button>
+        </div>
+    )
+  }
+
+}
+
+const testGlob = () => {
+
+  glob`
+  html,
+  body {
+    background: light;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  `;
+
+  glob(`
+  html,
+  body {
+    background: light;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  `);
+
+  glob({
+    body: {
+      background: 'light'
+    }
+  })
+
+}
