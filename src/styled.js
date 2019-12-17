0e2884cd6b40d7729c72472dee51233570a75ad5
Dony Sukardi <donysukardi@gmail.com>
2019-12-17 17:10:54 +0800

    Add setup function for pragma, forwardRef, and theme

--- src/styled.js
+++ src/styled.js
@@ -1,7 +1,11 @@
 import { css } from './css';
 
-let h;
-const setPragma = pragma => (h = pragma);
+let h, forwardRef, useTheme;
+const setup = (_h, _forwardRef, _useTheme = () => {}) => {
+    h = _h;
+    forwardRef = _forwardRef;
+    useTheme = _useTheme;
+};
 
 /**
  * Styled function
@@ -9,21 +13,25 @@ const setPragma = pragma => (h = pragma);
  */
 function styled(tag) {
     const _ctx = this || {};
-
     return function() {
         const _args = arguments;
-
-        return function Styled(props) {
-            const _props = (_ctx.p = Object.assign({}, props));
+        function Styled(props, ref) {
+            const theme = useTheme();
+            const _props = (_ctx.p = Object.assign(theme ? { theme } : {}, props));
             const _previousClassName = _props.className;
 
             _ctx.o = /\s*go[0-9]+/g.test(_previousClassName);
             _props.className =
                 css.apply(_ctx, _args) + (_previousClassName ? ' ' + _previousClassName : '');
+            if (ref) {
+                _props.ref = ref;
+            }
 
             return h(tag, _props);
-        };
+        }
+
+        return forwardRef ? forwardRef(Styled) : Styled;
     };
 }
 
-export { styled, setPragma };
+export { styled, setup };
