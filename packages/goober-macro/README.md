adb4b14880db2c04dd1a57c7732480515ad5fe25
Hadeeb Farhan <hadeebfarhan1@gmail.com>
2020-01-22 11:16:09 +0530

    switch to named imports, supports other imports from goober

---
+++ packages/goober-macro/README.md
@@ -0,0 +1,22 @@
+# goober babel macro
+
+`babel-plugin-macros` alternative to `babel-plugin-transform-goober`.
+
+## Prerequisites
+
+-   [`babel-plugin-macros`](https://github.com/kentcdodds/babel-plugin-macros)
+
+> Create React App already support `babel-plugin-macros`
+
+## Usage
+
+```js
+import styled from 'goober/macro';
+
+const Button = styled.button`
+    margin: 0;
+    padding: 1rem;
+    font-size: 1rem;
+    background-color: tomato;
+`;
+```
--- packages/goober-macro/index.js
+++ packages/goober-macro/index.js
@@ -1,4 +1,4 @@
-const { createMacro } = require('babel-plugin-macros');
+const { createMacro, MacroError } = require('babel-plugin-macros');
 const { addNamed } = require('@babel/helper-module-imports');
 
 module.exports = createMacro(gooberMacro);
@@ -6,9 +6,13 @@ module.exports = createMacro(gooberMacro);
 function gooberMacro({ references, babel, state }) {
     const program = state.file.path;
 
-    // Inject import {styled} from 'goober'
+    if (references.default) {
+        throw new MacroError('goober.macro does not support default import');
+    }
+
+    // Inject import {...} from 'goober'
     Object.keys(references).forEach(refName => {
-        const id = addNamed(program, 'styled', 'goober');
+        const id = addNamed(program, refName, 'goober');
         references[refName].forEach(referencePath => {
             referencePath.node.name = id.name;
         });
@@ -16,7 +20,9 @@ function gooberMacro({ references, babel, state }) {
 
     const t = babel.types;
 
-    references.default.forEach((referencePath, i) => {
+    const styledReferences = references.styled || [];
+
+    styledReferences.forEach(referencePath => {
         const type = referencePath.parentPath.type;
 
         if (type === 'MemberExpression') {
@@ -24,7 +30,7 @@ function gooberMacro({ references, babel, state }) {
             const functionName = node.object.name;
             let elementName = node.property.name;
 
-            // Custom elements
+            // Support custom elements
             if (/[A-Z]/.test(elementName)) {
                 elementName = elementName.replace(/[A-Z]/g, '-$&').toLowerCase();
             }
