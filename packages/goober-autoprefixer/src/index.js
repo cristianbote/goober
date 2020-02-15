b9c1c04e070de699266c090773afd08a183c9fac
jdecroock <decroockjovi@gmail.com>
2020-02-15 14:40:18 +0100

    golf bytes

--- packages/goober-autoprefixer/src/index.js
+++ packages/goober-autoprefixer/src/index.js
@@ -2,16 +2,10 @@ import { prefixproperty, prefixValue } from 'tiny-css-prefixer';
 import { opts } from 'goober';
 
 opts.p = (prop, value) => {
-    let css = `${prop}: ${value};\n`;
-    const prefixedValue = prefixValue(css, css);
-    if (css !== prefixedValue) {
-        return prefixedValue;
-    }
-
+    let css = prefixValue(prop, value) + ';\n';
     const flag = prefixproperty(prop);
     if (flag & 0b001) css += `-ms-${css}`;
     if (flag & 0b010) css += `-moz-${css}`;
     if (flag & 0b100) css += `-webkit-${css}`;
-
     return css;
 };
