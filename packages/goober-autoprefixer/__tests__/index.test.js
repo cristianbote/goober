23847a2c1a07fffad12bc5340028ef839c1b7bdc
jovi De Croock <decroockjovi@gmail.com>
2020-02-17 14:13:14 +0100

    test correctly

--- packages/goober-autoprefixer/__tests__/index.test.js
+++ packages/goober-autoprefixer/__tests__/index.test.js
@@ -1,18 +1,18 @@
 import '../src';
 import { parse } from '../../../src/core/parse';
 
-const sanitize = str => str.replace('\n', '');
+const sanitize = str => str.replace(/\s/g, '');
 
 describe('goober-autoprefixer', () => {
     it('can prefix values', () => {
         const output = parse({ position: 'sticky' }, 'div');
-        expect(sanitize(output)).toEqual('div{position: -webkit-sticky, sticky;}');
+        expect(sanitize(output)).toEqual('div{position:-webkit-sticky,sticky;}');
     });
 
     it('can prefix properties', () => {
         const output = parse({ appearance: 'none' }, 'div');
-        expect(sanitize(output)).toEqual(`div{appearance: none;-moz-appearance: none;
--webkit-appearance: none;
-}`);
+        expect(sanitize(output)).toEqual(
+            `div{appearance:none;-moz-appearance:none;-webkit-appearance:none;}`
+        );
     });
 });
