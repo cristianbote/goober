0ae946b6647cae7eb255f90773c700b50eedcda2
Russell <russelgal@gmail.com>
2020-09-07 16:31:40 +0500

    Update astish.js

--- src/core/astish.js
+++ src/core/astish.js
@@ -1,5 +1,5 @@
 let newRule = /(?:([a-z0-9-%@]+) *:? *([^{;]+?);|([^;}{]*?) *{)|(})/gi;
-let ruleClean = /\/\*[\s\S]*?\*\/|\s{2,}|\n|\/\/.*\n/gm;
+let ruleClean = /\/\*[\s\S]*?\*\/|\s{2,}|\n|\s??\/\/.*\n/gm;
 
 /**
  * Convert a css style string into a object
