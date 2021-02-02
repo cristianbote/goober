0f011e99c4eadaaec6746ccf0ffbc4ec52b7dbd0
cristianbote <botecristian@yahoo.com>
2021-02-02 09:36:45 +0200

    (bug) Fix the sticky prefixer for position

--- prefixer/__tests__/index.test.js
+++ prefixer/__tests__/index.test.js
@@ -5,7 +5,7 @@ const sanitize = (str) => str.replace(/\s/g, '');
 describe('goober-autoprefixer', () => {
     it('can prefix values', () => {
         const output = prefix('position', 'sticky');
-        expect(sanitize(output)).toEqual('position:-webkit-sticky,sticky;');
+        expect(sanitize(output)).toEqual('position:-webkit-sticky;position:sticky;');
     });
 
     it('can prefix properties', () => {
