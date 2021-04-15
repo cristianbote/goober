0dfe7baac1922894e2f06aed44eafeebe3b236d2
Ilham Wahabi <wahabiputra@gmail.com>
2021-04-15 13:52:23 +0700

    Remove some variable usage (#289)
    * Remove variable 'l' in to-hash
    
    * Remove 'sheet' in get-sheet

--- src/core/get-sheet.js
+++ src/core/get-sheet.js
@@ -12,14 +12,13 @@ export let getSheet = (target) => {
     if (typeof window !== 'undefined') {
         // Querying the existing target for a previously defined <style> tag
         // We're doing a querySelector because the <head> element doesn't implemented the getElementById api
-        let sheet = target ? target.querySelector('#' + GOOBER_ID) : window[GOOBER_ID];
-        if (!sheet) {
-            // Note to self: head.innerHTML +=, triggers a layout/reflow. Avoid it.
-            sheet = (target || document.head).appendChild(document.createElement('style'));
-            sheet.innerHTML = ' ';
-            sheet.id = GOOBER_ID;
-        }
-        return sheet.firstChild;
+        return (
+            (target ? target.querySelector('#' + GOOBER_ID) : window[GOOBER_ID]) ||
+            Object.assign((target || document.head).appendChild(document.createElement('style')), {
+                innerHTML: ' ',
+                id: GOOBER_ID
+            })
+        ).firstChild;
     }
     return target || ssr;
 };
