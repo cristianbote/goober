426b1fa618a4d8da9019412778173d766092695e
James Berry <jb@jamesbe.com>
2021-07-08 16:48:21 +0100

    added useTheme type

--- goober.d.ts
+++ goober.d.ts
@@ -63,6 +63,7 @@ declare namespace goober {
         tag: CSSAttribute | TemplateStringsArray | string,
         ...props: Array<string | number>
     ): string;
+    const useTheme: () => DefaultTheme | undefined;
     type StyledVNode<T> = (props: T, ...args: any[]) => any;
     type StylesGenerator<P extends Object = {}> = (props: P) => CSSAttribute | string;
     type Tagged<P extends Object = {}> = <PP extends Object = {}>(
