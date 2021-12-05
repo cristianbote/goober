8774488e1100672ff7fd152ccfce52ef1f961341
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-05 23:40:10 +0530

    Replace undefined with null

--- src/core/parse.js
+++ src/core/parse.js
@@ -43,7 +43,7 @@ export let parse = (obj, selector) => {
                       })
                     : key
             );
-        } else if (val != undefined) {
+        } else if (val != null) {
             // If this isn't an empty rule
             key = key.replace(/[A-Z]/g, '-$&').toLowerCase();
             // Push the line for this property
