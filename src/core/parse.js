f094523cdaa913cf5b8f7bf9546523a7d845f98d
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-12-05 21:23:42 +0530

    Update parse.js

--- src/core/parse.js
+++ src/core/parse.js
@@ -12,52 +12,46 @@ export let parse = (obj, selector) => {
     for (let key in obj) {
         let val = obj[key];
 
-        // If this isn't an empty rule
-        if (val != undefined && val != null) {
-            // If this is a 'block'
-            if (typeof val == 'object') {
-                // If these are the `@` rule
-                if (key[0] == '@') {
-                    // Handling the `@font-face` where the
-                    // block doesn't need the brackets wrapped
-                    if (key[1] == 'f') {
-                        blocks += parse(val, key);
-                    } else {
-                        // Regular rule block
-                        blocks += key + '{' + parse(val, key[1] == 'k' ? '' : selector) + '}';
-                    }
-                } else {
-                    // Call the parse for this block
-                    blocks += parse(
-                        val,
-                        selector
-                            ? // Go over the selector and replace the matching multiple selectors if any
-                              selector.replace(/([^,])+/g, (sel) => {
-                                  // Return the current selector with the key matching multiple selectors if any
-                                  return key.replace(/(^:.*)|([^,])+/g, (k) => {
-                                      // If the current `k`(key) has a nested selector replace it
-                                      if (/&/.test(k)) return k.replace(/&/g, sel);
-
-                                      // If there's a current selector concat it
-                                      return sel ? sel + ' ' + k : k;
-                                  });
-                              })
-                            : key
-                    );
-                }
+        if (key[0] == '@') {
+            // If these are the `@` rule
+            if (key[1] == 'i') {
+                // Handling the `@import`
+                outer = key + ' ' + val + ';';
+            } else if (key[1] == 'f') {
+                // Handling the `@font-face` where the
+                // block doesn't need the brackets wrapped
+                blocks += parse(val, key);
             } else {
-                if (/^@i/.test(key)) {
-                    outer = key + ' ' + val + ';';
-                } else {
-                    key = key.replace(/[A-Z]/g, '-$&').toLowerCase();
-                    // Push the line for this property
-                    current += parse.p
-                        ? // We have a prefixer and we need to run this through that
-                          parse.p(key, val)
-                        : // Nope no prefixer just append it
-                          key + ':' + val + ';';
-                }
+                // Regular at rule block
+                blocks += key + '{' + parse(val, key[1] == 'k' ? '' : selector) + '}';
             }
+        } else if (typeof val == 'object') {
+            // Call the parse for this block
+            blocks += parse(
+                val,
+                selector
+                    ? // Go over the selector and replace the matching multiple selectors if any
+                      selector.replace(/([^,])+/g, (sel) => {
+                          // Return the current selector with the key matching multiple selectors if any
+                          return key.replace(/(^:.*)|([^,])+/g, (k) => {
+                              // If the current `k`(key) has a nested selector replace it
+                              if (/&/.test(k)) return k.replace(/&/g, sel);
+
+                              // If there's a current selector concat it
+                              return sel ? sel + ' ' + k : k;
+                          });
+                      })
+                    : key
+            );
+        } else if (val != undefined && val != null) {
+            // If this isn't an empty rule
+            key = key.replace(/[A-Z]/g, '-$&').toLowerCase();
+            // Push the line for this property
+            current += parse.p
+                ? // We have a prefixer and we need to run this through that
+                  parse.p(key, val)
+                : // Nope no prefixer just append it
+                  key + ':' + val + ';';
         }
     }
 
