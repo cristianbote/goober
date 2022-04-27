69af09ec54bbf7f225294292e3094f07ec4d8336
cristianbote <botecristian@yahoo.com>
2022-04-27 14:16:27 +0300

    Smaller

--- src/core/astish.js
+++ src/core/astish.js
@@ -1,5 +1,5 @@
 let newRule = /(?:([\u0080-\uFFFF\w-%@]+) *:? *([^{;]+?);|([^;}{]*?) *{)|(}\s*)/g;
-let ruleClean = /\/\*[^]*?\*\//g;
+let ruleClean = /\/\*[^]*?\*\/|  +/g;
 
 /**
  * Convert a css style string into a object
@@ -8,23 +8,17 @@ let ruleClean = /\/\*[^]*?\*\//g;
  */
 export let astish = (val) => {
     let tree = [{}];
-    let block;
-    let selector;
-
-    function clean(val) {
-        return !val ? '' : val.trim().replace(/\s+|\n+/g, ' ');
-    }
+    let block, left;
 
     while ((block = newRule.exec(val.replace(ruleClean, '')))) {
         // Remove the current entry
-
         if (block[4]) {
             tree.shift();
         } else if (block[3]) {
-            selector = clean(block[3]);
-            tree.unshift((tree[0][selector] = tree[0][selector] || {}));
+            left = block[3].replace(/\n+/g, ' ').trim();
+            tree.unshift((tree[0][left] = tree[0][left] || {}));
         } else {
-            tree[0][clean(block[1])] = clean(block[2]);
+            tree[0][block[1]] = block[2].replace(/\n+/g, ' ').trim();
         }
     }
 
