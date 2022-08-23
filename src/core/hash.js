3b74e2b1d39a8936dd074dd36f7d75a1e56c3b7c
Looky1173 <lgl1173and2006@gmail.com>
2022-08-23 19:02:12 +0200

    Fix cristianbote/goober#376

--- src/core/hash.js
+++ src/core/hash.js
@@ -53,8 +53,14 @@ export let hash = (compiled, sheet, global, append, keyframes) => {
         );
     }
 
+    // If the global flag is set, save the current stringified and compiled CSS to `cache.g`
+    // to allow replacing styles in <style /> instead of appending them.
+    // This is required for using `createGlobalStyles` with themes
+    let cssToReplace = global && cache.g ? cache.g : null;
+    if (global) cache.g = cache[className];
+
     // add or update
-    update(cache[className], sheet, append);
+    update(cache[className], sheet, append, cssToReplace);
 
     // return hash
     return className;
